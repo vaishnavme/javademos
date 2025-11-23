db7e7e224d69092d2381998e8db0e6b758ea6521
Vaishnav <chandurkarvaishnav@gmail.com>
2025-11-23 11:55:11 +0530

    java demos project setup

---
+++ src/main/java/com/vaishnavs/javademos/JavademosApplication.java
@@ -0,0 +1,13 @@
+package com.vaishnavs.javademos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavademosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavademosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaishnavs/javademos/JavademosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaishnavs.javademos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavademosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
