8b5fac591588ce7dd70d6ac8702b30677c62ba91
Vaishnav Chandurkar <42497931+vaishnavme@users.noreply.github.com>
2025-11-24 16:56:24 +0530

    feat: request concurrency  (#4)

--- src/main/java/com/vaishnavs/javademos/repository/PostsRepository.java
+++ src/main/java/com/vaishnavs/javademos/repository/PostsRepository.java
@@ -1,8 +1,19 @@
 package com.vaishnavs.javademos.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.vaishnavs.javademos.model.PostsEntity;
 
+import jakarta.persistence.LockModeType;
+
 public interface PostsRepository extends JpaRepository<PostsEntity, Integer> {
+
+  @Lock(LockModeType.PESSIMISTIC_WRITE)
+  @Query("SELECT p FROM PostsEntity p WHERE p.id = :id")
+  Optional<PostsEntity> findByIdWithLock(@Param("id") int id);
 }
